f4b1d798c0f3d69ce2c8eef723f4a223bd5ded2a
Vũ Quang Duy <vduy2350@gmail.com>
2022-08-31 10:25:08 +0700

    update

--- index.js
+++ index.js
@@ -179,6 +179,19 @@ function countWords(str) {
 console.log(countWords("   xin chao cac ban"));
 
 // String 02
+function statisticsWords(str) {
+    let result = str.trim().split(" ").reduce((a,b) => {
+        if(b != '') {
+            a[b] = a[b] + 1 || 1
+        }
+        return a
+    }, {})
+    return result
+}
+
+console.log(statisticsWords("easy      frontend          easy"));
+
+
 // String 03
 function statisticsCharacters(str) {
     const strArr = str.split('')

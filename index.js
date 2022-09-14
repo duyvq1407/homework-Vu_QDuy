5e6278af44b266ca551704a03ddfea82aabc765c
duyvq1407 <duyvq1407@gmail.com>
2022-09-14 09:21:18 +0700

    Promise

---
+++ index.js
@@ -0,0 +1,51 @@
+class Queue{
+    constructor(delay = 1000){
+        this.delay = delay;
+        this.callbackQueue = [];
+    }
+
+    add(callback){
+        if (this.callbackQueue.length > 1) {
+            setTimeout(callback, this.delay)
+            this.callbackQueue.shift()
+        } else {
+            this.callbackQueue.push(callback);
+            callback();
+            this.callbackQueue.shift()
+        }
+    }
+}
+
+const a = new Queue();
+a.add(()=>console.log('callback'))
+a.add(()=>console.log('callback2'))
+a.add(()=>console.log('callback3'))
+
+
+
+const getApi = (keys) => {
+  let errorKeys = []
+  let listkey = [...keys];
+
+  while (listkey.length) {
+    let requests = listkey.splice(0, 5).map((key) => fetch(`https://api.github.com/search/repositories?q=${key}`))
+    Promise.allSettled(requests)
+    .then((results) => {
+      results.forEach((result) => {
+        if (result.status === "fulfilled") {
+          if (result.value.ok) {
+            result.value.json().then(res => console.log(res.items[0]));
+          }
+        }
+        if (result.status === "rejected" || !result.value.ok) {
+          errorKeys.push(result.value.url)
+        }
+      });
+      return errorKeys
+    }).then(res => {getApi(res)})
+    
+  }
+  };  
+
+let keys = ['swift','rust','javascript','react', 'rx','ruby','rails','php','objective-c','html','css','pug']
+getApi(keys)
